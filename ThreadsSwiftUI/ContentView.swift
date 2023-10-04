a3c00816219bd7a12ca8d0756cef66662de78b75
パクギョンソク <boardguy1024@gmail.com>
2023-10-04 21:34:02 +0900

    Initial Commit

---
+++ ThreadsSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThreadsSwiftUI
+//
+//  Created by パクギョンソク on 2023/10/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThreadsSwiftUI/ThreadsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsSwiftUIApp.swift
+//  ThreadsSwiftUI
+//
+//  Created by パクギョンソク on 2023/10/04.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
